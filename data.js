9a4fb97af080a389ebadd9148d40a947a08b7b3e
Jen Nguyen <jenniferannnguyen@live.com>
2023-05-25 20:22:04 -0600

    Feature letsCook function working

---
+++ data.js
@@ -0,0 +1,98 @@
+// var sideDishes = [
+//   "Miso Glazed Carrots",
+//   "Coleslaw",
+//   "Garden Salad",
+//   "Crispy Potatoes",
+//   "Sweet Potato Tots",
+//   "Coconut Rice",
+//   "Caeser Salad",
+//   "Shrimp Summer Rolls",
+//   "Garlic Butter Mushrooms",
+//   "Hush Puppies"
+// ]
+
+// var mainDishes = [
+//   "Spaghetti and Meatballs",
+//   "Pineapple Chicken",
+//   "Shakshuka",
+//   "Thai Yellow Curry",
+//   "Bibimbap",
+//   "Chicken Parmesean",
+//   "Butternut Squash Soup",
+//   "BBQ Chicken Burgers",
+//   "Ramen",
+//   "Empanadas",
+//   "Chicken Fried Rice",
+//   "Sheet Pan Fajitas",
+//   "Margarita Pizza",
+// ]
+
+// var desserts = [
+// "Apple Pie",
+// "Lemon Meringue Pie",
+// "Black Forest Cake",
+// "Banana Bread",
+// "Peach Cobbler",
+// "Cheesecake",
+// "Funfetti Cake",
+// "Baklava",
+// "Flan",
+// "Macarons",
+// "Macaroons",
+// "Chocolate Cupcakes",
+// "Pavlova",
+// "Pumpkin Pie",
+// "Key Lime Pie",
+// "Tart Tatin",
+// "Croissants",
+// "Eclairs"
+
+
+
+var sideDishes = [
+  "Bánh bao",
+  "Bánh bột lọc",
+  "Bánh rán",
+  "Bánh bột lọc lá",
+  "Bánh rán",
+  "Bánh khúc",
+  "Bánh căn",
+  "Bánh xèo",
+  "Bánh cuốn",
+  "Chả giò",
+  "Gỏi cuốn",
+  "Bánh lá",
+  ]
+
+var mainDishes = [
+  "Congee",
+  "Thit Kho Tau",
+  "Bò kho",
+  "Cơm nắm",
+  "Bún Chả",
+  "phở",
+  "Bò Lúc Lắc",
+  "Gà Bắp Cải",
+  "Gỏi Gà Bắp Cải",
+  "Bánh mi", 
+  "Bún bò Huế",
+  "Thịt Nướng",
+  "Bún Gà Nướng",
+  "Cơm tấm",
+  "Chả trứng",
+]
+
+var desserts = [
+  "Xôi",
+  "Bánh bò",
+  "Bánh cáy",
+  "Bánh da lợn",
+  "Chè Ba Màu",
+  "Bánh flan",
+  "Bánh kẹp lá dứa",
+  "Bánh khoai mì",
+  "Bánh nhãn",
+  "Bánh tiêu",
+  "thạch sương sáo",
+  "chè chuối"
+]
---
+++ main.js
@@ -0,0 +1,52 @@
+var letsCookButton = document.querySelector('.button');
+var sideDishRadio = document.querySelector('.side-dish-radio');
+var mainDishRadio = document.querySelector('.main-dish-radio');
+var dessertRadio = document.querySelector('.desserts-radio');
+var potImage = document.querySelector('img')
+// var recipeBox = document.querySelector('.recipe-box')
+var makeDish = document.querySelector('#hidden-text');
+var recipeBox = document.querySelector('.recipe');
+
+
+letsCookButton.addEventListener('click', letsCook)
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function letsCook() {
+  console.log("when hidden:", makeDish)
+  potImage.classList.add('hidden');
+  // makeDish.classList.remove('hidden')
+  // makeDish.classList.add('visible');
+  if (sideDishRadio.checked) {
+    recipeBox.innerText = 'You should make'
+    makeDish.innerText = sideDishes[getRandomIndex(sideDishes)];
+  } else if (dessertRadio.checked) {
+    recipeBox.innerHTML = 'You should make'
+    makeDish.innerText = desserts[getRandomIndex(desserts)];
+  } else {
+    recipeBox.innerHTML = 'You should make'
+    makeDish.innerText = mainDishes[getRandomIndex(mainDishes)];
+  }
+}
+
+
+
+
+
+
+// function letsCook() {
+//   potImage.classList.add('hidden')
+//   if (sideDishRadio.checked) {
+//     recipeBox.innerText = sideDishes[getRandomIndex(sideDishes)]
+//   } else if (mainDishRadio.checked) {
+//     recipeBox.innerText = mainDish[getRandomIndex(mainDishes)]
+// } else {
+//     recipeBox.innerText = desserts[getRandomIndex(desserts)]
+// }
+// }
+    
+
+
+
