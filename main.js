564233bf2b187816be91b5989611cd326175a8bd
Jen Nguyen <jenniferannnguyen@live.com>
2023-05-28 16:53:31 -0600

    Added checkInput function for radio buttons with disable feature

--- main.js
+++ main.js
@@ -7,20 +7,34 @@ var makeDish = document.querySelector('.suggested-meal');
 var suggestionBox = document.querySelector('.you-should-make');
 var loading = document.querySelector('.loader-container')
 
-letsCookButton.addEventListener('click', letsCook)
+letsCookButton.addEventListener('click', function() {
+  suggestionBox.classList.remove('fade-in');
+  makeDish.classList.remove('fade-in')
+  suggestionBox.offsetWidth;
+  makeDish.offsetWidth; 
+  suggestionBox.classList.add('fade-in');
+  makeDish.classList.add('fade-in')
+});
+
+// letsCookButton.addEventListener('click', letsCook)
+letsCookButton.addEventListener('click', function(event) {
+    letsCook(event)
+})
+sideDishRadio.addEventListener('click', checkInput);
+mainDishRadio.addEventListener('click', checkInput);
+dessertRadio.addEventListener('click', checkInput);
+
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-function letsCook() {
+function letsCook(event) {
   loading.classList.remove('hidden');
   potImage.classList.add('hidden');
   suggestionBox.innerText = '';
   makeDish.innerText = '';
-  
   setTimeout(function() {
-  
     if (sideDishRadio.checked) {
       suggestionBox.innerText = 'You should make:'
       makeDish.innerText = sideDishes[getRandomIndex(sideDishes)];
@@ -35,6 +49,7 @@ function letsCook() {
 
   }, 1000 );
 }
+  
 
 setTimeout(function() {
   var make = document.getElementById('make');
@@ -46,7 +61,12 @@ setTimeout(function() {
   suggestion.style.opacity = 1;
 }, 2000);
 
-
+function checkInput() {
+  if (sideDishRadio.checked || mainDishRadio.checked || dessertRadio.checked) {
+   letsCookButton.removeAttribute('disabled') 
+  }
+}
+  
     
 
 
